508df6847c17d435d76f4ce3dc176bef5696e562
Julian <wit.ju01@gmail.com>
2019-02-06 22:09:07 +0100

    HUD now extends NotMoving

--- src/HUD.java
+++ src/HUD.java
@@ -7,7 +7,7 @@ public class HUD extends NotMoving {
     GreenfootImage Background = new GreenfootImage("StatBars.png");
 
 
-    public HUD (){
+    public HUD(){
         setImage(Background);
     }
     //TODO bars need do sync with actual values
@@ -17,26 +17,22 @@ public class HUD extends NotMoving {
         setImage(Background);
         if (getWorld().getObjects(Player.class).get(0)!= null) {
            Player p = getWorld().getObjects(Player.class).get(0);
-           healthBar(p);
-           enduranceBar( p);
+            drawStatBars(p);
         }else{
             System.out.println("No Player found");
         }
     }
 
-    private void healthBar(Player p){
+    private void drawStatBars(Player p){
         int health = p.getLife();
         Background.setColor(Color.RED);
         Background.fillRect(50,20, health,27);
 
-    }
-    private void enduranceBar(Player p){
         double endurance = p.getEndurance();
         endurance = endurance*0.18;
         Background.setColor(Color.GREEN);
         Background.fillRect(60,60, (int)endurance,27);
     }
-
 
     //shows Text on screen|not working|may be used for WeaponNameDisplay
     public void fontTest(Graphics itemName, String name){
