bf3fb4a016b17a4c8dacaf27e004520bcc8e9c86
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-02-07 20:43:59 +0100

    small improvements.

--- src/Hostile.java
+++ src/Hostile.java
@@ -2,4 +2,16 @@ public abstract class Hostile extends NPC {
     public void attack(Attackable actor,int damage){
         actor.setLife(actor.getLife()-damage);
     }
+    public void moveToPlayer(int visualRange){
+        Player player = getPlayer(visualRange);
+        if(player != null) {
+            moveInDirectionOf(player);
+        }
+    }
+    public void attackPlayer(int attackRange,int damage){
+        Player player = getPlayer(attackRange);
+        if(player!=null){
+            attack(player,damage);
+        }
+    }
 }
