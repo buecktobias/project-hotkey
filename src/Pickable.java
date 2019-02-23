dd5586767a4d60122fa597ad9d7cbd2beaaae16a
Julian <wit.ju01@gmail.com>
2019-02-23 15:38:57 +0100

    Merge branch 'master' of D:\Ordner\Schule\Info\ProjectHotkey\ProjectHotkey with conflicts.

---
+++ src/Pickable.java
@@ -0,0 +1,5 @@
+public interface Pickable {
+    int count = 0;
+    boolean pickedUp = false;
+
+}
--- src/Staff.java
+++ src/Staff.java
@@ -1,8 +1,12 @@
-public class Staff extends Weapon {
+public class Staff extends Weapon implements Pickable {
 
     private double damage;
     private int attackSpeed;
     private int attackRange;
+
+    public int count;
+    public final int id = 1;
+
 
     public Staff(int attackRange) {
         this.attackRange = attackRange;
@@ -11,5 +15,13 @@ public class Staff extends Weapon {
     }
     public void act() {
 
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 }
