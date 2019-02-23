ce0011dd58f5c7e9200f7702c4376582f5132100
Julian <wit.ju01@gmail.com>
2019-02-23 21:32:05 +0100

    New InventoryScreen...again!!!!

--- src/Inventory.java
+++ src/Inventory.java
@@ -4,30 +4,32 @@ import greenfoot.GreenfootImage;
 import java.awt.*;
 import java.util.LinkedList;
 
+
 public class Inventory extends Actor implements Fixed {
     private Player p;
     private LinkedList<Pickable> items;
     private int capacity;
-    GreenfootImage InventoryScreen = new GreenfootImage("images/MyInventoryV1.png");
+    GreenfootImage InventoryScreen = new GreenfootImage("images/MyInventoryV2.png");
 
     public Inventory(Player p){
         this.p = p;
         this.capacity = 42;
         setImage(InventoryScreen);
+        getItems(p);
     }
     public void drawItems(){
 
     }
-    public void getItems(){
-
+    public void getItems(Player p){
+        items = p.getInventory();
     }
 
     //shows Text on screen|not working|may be used for WeaponNameDisplay
-    public void fontTest(Graphics itemName, String name){
-        itemName.drawString(name, 100,100);
+    public void fontTest(Graphics g, String name){
+        g.drawString(name, 100,100);
         Font equipedWeapon = new Font("Arial", Font.BOLD, 12);
-        itemName.setColor(Color.BLACK);
-        itemName.setFont(equipedWeapon);
+        g.setColor(Color.BLACK);
+        g.setFont(equipedWeapon);
     }
 
     //Getters and Setters
