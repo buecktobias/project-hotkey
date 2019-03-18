7cef22042f74afa550523da178ae4439c976637c
Olowain <wit.ju01@gmail.com>
2019-03-18 23:14:23 +0100

    small improvement: Hud now less redundant

--- src/HUD.java
+++ src/HUD.java
@@ -13,34 +13,21 @@ public class HUD extends Actor implements Fixed {
         this.p = p;
     }
 
-
     public void act(){
         Background.clear();
         Background = new GreenfootImage("images/Hud_Menu_Images/Hud_V2.png");
         setImage(Background);
-        drawHealthBar();
-        drawEnduranceBar();
+        drawBarAt(p.getLife(), p.getMaxLife(), 308, "#7f0000", "#990000", "#cc0000", 12, 12);
+        drawBarAt(p.getEndurance(), p.getMaxEndurance(), 246, "#007f00", "#009900", "#00cc00",12,42 );
     }
-    private void drawHealthBar(){
-        double health = p.getLife();
-        int healthBarWidth = (int) (health / (double) p.getMaxLife() * 280);
-        Background.setColor(Color.decode("#7f0000"));
-        Background.fillRect(12,12, healthBarWidth,23);
-        Background.setColor(Color.decode("#990000"));
-        Background.fillRect(12,12, healthBarWidth,16);
-        Background.setColor(Color.decode("#cc0000"));
-        Background.fillRect(12,12, healthBarWidth,9);
+    private void drawBarAt(double width, int maxValue, int scale, String lightC, String medC, String darkC, int X, int Y){
+        int scaleWidth = (int) (width / (double) maxValue * scale);
+        Background.setColor(Color.decode(lightC));
+        Background.fillRect(X,Y, scaleWidth,23);
+        Background.setColor(Color.decode(medC));
+        Background.fillRect(X,Y, scaleWidth,16);
+        Background.setColor(Color.decode(darkC));
+        Background.fillRect(X,Y, scaleWidth,9);
     }
-    private void drawEnduranceBar(){
-        double endurance = p.getEndurance();
-        int enduranceBarWidth = (int) (endurance / (double) p.getMaxEndurance() * 180);
-        Background.setColor(Color.decode("#007f00"));
-        Background.fillRect(12,42, enduranceBarWidth,23);
-        Background.setColor(Color.decode("#009900"));
-        Background.fillRect(12,42, enduranceBarWidth,16);
-        Background.setColor(Color.decode("#00cc00"));
-        Background.fillRect(12,42, enduranceBarWidth,9);
-    }
-
     //Getters and Setters
 }
