b3fb8faa8be45361d3b503631e34bd2e513a82bb
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-03-20 10:42:46 +0100

    Cobwebs hold Movingactors

--- src/Cobweb.java
+++ src/Cobweb.java
@@ -1,13 +1,23 @@
 import greenfoot.GreenfootImage;
 
-public class Cobweb extends Environment {
+public class Cobweb extends Environment implements HasEffect {
     private GreenfootImage defaultImage = new GreenfootImage("images/Environment/cobweb.png");
+    private int health = 20;
     public Cobweb(){
         defaultImage.scale(32,32);
     }
 
     @Override
+    public void effects(MovingActor movingActor) {
+        movingActor.setSpeed(0);
+        health--;
+    }
+
+    @Override
     public void act() {
         setImage(defaultImage);
+        if(health < 0){
+            getWorld().removeObject(this);
+        }
     }
 }
