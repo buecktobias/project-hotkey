a4b5ad2c9013cac37bfb42d5aaf4784c1dad3754
Julian <wit.ju01@gmail.com>
2019-03-20 09:56:36 +0100

    Bow image now correctly aligned

--- src/GUI.java
+++ src/GUI.java
@@ -4,6 +4,7 @@ import greenfoot.GreenfootImage;
 import java.awt.*;
 
 public abstract class GUI extends Actor {
+
     public void drawItemAt(GreenfootImage g, int X, int Y, Item item){
         if(item != null){
             g.setColor(Color.WHITE);
@@ -13,5 +14,14 @@ public abstract class GUI extends Actor {
             g.drawRect(X + 1,Y +1, 54, 54);
             g.drawImage(item.getItemImage(), X, Y);
         }
+    }
+    public void drawBarAt(GreenfootImage g, double width, int maxValue, int scale, String lightC, String medC, String darkC, int X, int Y){
+        int scaleWidth = (int) (width / (double) maxValue * scale);
+        g.setColor(Color.decode(lightC));
+        g.fillRect(X,Y, scaleWidth,23);
+        g.setColor(Color.decode(medC));
+        g.fillRect(X,Y, scaleWidth,16);
+        g.setColor(Color.decode(darkC));
+        g.fillRect(X,Y, scaleWidth,9);
     }
 }
