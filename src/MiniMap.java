a50029ab3d11cf4968c23a1fe9f3fec4c6b17451
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-03-24 18:51:47 +0100

    Improved MiniMap

--- src/MiniMap.java
+++ src/MiniMap.java
@@ -3,11 +3,14 @@ import greenfoot.GreenfootImage;
 import java.util.List;
 
 public class MiniMap extends Window {
-    private int updateFrames = 20;
+    private int updateFrames = 5;
     private long lastFrameUpdatet = -99999;
+    private final int sizeMap = 2000;
+    private final int width = 200;
+    private final int height = 200;
     private GreenfootImage defaultImage = new GreenfootImage("images/Screens/background_grass.png");
     public MiniMap(){
-        defaultImage.scale(200,200);
+        defaultImage.scale(width,height);
         setImage(defaultImage);
     }
 
@@ -15,19 +18,27 @@ public class MiniMap extends Window {
     public void act() {
         if(FPS.getFrame() - lastFrameUpdatet > updateFrames){
             lastFrameUpdatet = FPS.getFrame();
-            System.out.println("lol");
+            Player player = Player.getInstance();
+            final int playerX = player.getX();
+            final int playerY = player.getY();
             List<Entity> entityList = getWorld().getObjects(Entity.class);
-            entityList.removeIf(entity -> entity.getX() > 1000 || entity.getX() < -1000 || entity.getY() > 1000 || entity.getY() < -1000);
+            entityList.removeIf(entity -> Math.abs(entity.getX() - playerX) > sizeMap|| Math.abs(entity.getY() - playerY) > sizeMap);
             GreenfootImage copyOfImage = new GreenfootImage(defaultImage);
             for(Entity entity:entityList){
-                int x = entity.getX();
-                int y = entity.getY();
-                int scaledX = x / 10 + 100;
-                int scaledY = y / 10 + 100;
+                if(entity instanceof Player){
+                    continue;
+                }
+                int xdifference =  entity.getX() - playerX ;
+                int ydifference = entity.getY() - playerY;
+                int scaledX = xdifference / (sizeMap/width) + width / 2;
+                int scaledY = ydifference / (sizeMap/height) + height / 2;
                 GreenfootImage entitysImage = new GreenfootImage(entity.getImage());
-                entitysImage.scale(Math.round(entitysImage.getWidth() / 10) + 1, Math.round(entitysImage.getHeight() / 10) + 1);
+                entitysImage.scale(Math.round(entitysImage.getWidth() / (sizeMap/width)) + 1, Math.round(entitysImage.getHeight() / (sizeMap/height)) + 1);
                 copyOfImage.drawImage(entitysImage,scaledX,scaledY);
             }
+            GreenfootImage playerImage = new GreenfootImage(player.getImage());
+            playerImage.scale(Math.round(playerImage.getWidth() / (sizeMap/width)) + 1, Math.round(playerImage.getHeight() / (sizeMap/height)) + 1);
+            copyOfImage.drawImage(playerImage,width / 2,height / 2);
             setImage(copyOfImage);
         }
 
