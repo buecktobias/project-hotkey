b3dbd76ccdcdb0ef9109ba864e0542ac2977adfa
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-04-02 15:09:23 +0200

    tidied Tree code

--- src/Tree.java
+++ src/Tree.java
@@ -1,13 +1,8 @@
 import greenfoot.GreenfootImage;
 
-import java.util.Random;
-
 public class Tree extends Environment implements Explodes{
-    private double rotation;
-    private boolean rotateLeft = false;
-    private GreenfootImage defaultImage = new GreenfootImage("images/Environment/pine_tree.png");
+    private GreenfootImage defaultImage = new GreenfootImage(Files.getENVIRONMENT_PATH() + "pine_tree.png");
     public Tree(){
-        rotation = new Random().nextInt(10)-5;
         setImage(defaultImage);
     }
 }
